fe7b442c72b40dbff4ec4301e8dd5c89bd483de5
Henrik Aronsson <henrik.aronsson.94@gmail.com>
2016-08-08 22:29:27 +0200

    goodnight

--- server.js
+++ server.js
@@ -16,24 +16,42 @@ var favicon = favicon(__dirname + '/media/favicon.ico');
 
 // Create a http server with a callback handling all requests
 var httpServer = http.createServer(function (req, res) {
+    console.log((new Date()) + ' Received request for ' + req.url);
+
     var done = finalhandler(req, res);
+    var params, dest;
+
     favicon(req, res, function onNext(err) {
       if (err) return done(err);
-      console.log((new Date()) + ' Received request for ' + req.url);
+      var params = req.url.split('/');
+      params = params.slice(1, params.length);
+      dest = params.shift();
+      console.log(dest);
       res.write("<html><head>");
       res.write("<title>Gymapp</title>");
-      res.write("</head><body>");
-      res.write("<h1>Hej Henrik!</h1>");
-      res.write("<table>");
-      db.all("SELECT ID, TEXT FROM data", function(err, rows) {
-          rows.forEach(function (row) {
-              res.write("<tr><td>" + row.ID + "</td><td>" + row.TEXT + "</tr>");
+      if (dest === 'setup') {
+          createTables();
+          res.end('</head><body><h1>Tabeller skapade.</h1><a href="/">Tillbaka</a></body></html>');
+      } else {
+          res.write("</head><body>");
+          res.write("<h1>Hej Henrik!</h1>");
+          res.write("<table>");
+          db.all("SELECT ID, TEXT FROM data", function(err, rows) {
+              rows.forEach(function (row) {
+                  res.write("<tr><td>" + row.ID + "</td><td>" + row.TEXT + "</tr>");
+              });
+              res.end('</body></html>');
           });
-          res.end('</body></html>');
-      });
+      }
     });
 });
 
 httpServer.listen(port, function () {
     console.log((new Date()) + ' HTTP server is listening on port ' + port);
 });
+
+function createTables() {
+    console.log("creating tables");
+    db.run('CREATE TABLE IF NOT EXISTS "PERSON" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "NAMN" VARCHAR NOT NULL  UNIQUE )');
+    db.run('CREATE TABLE IF NOT EXISTS "OVNINGAR" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "NAMN" VARCHAR NOT NULL  UNIQUE )');
+}
