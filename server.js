b78a9a53335282bc6e7256827dc9b4b96e32d3d2
Henrik Aronsson <henrik.aronsson.94@gmail.com>
2016-08-10 23:51:54 +0200

    Great work with actually commiting...

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var rq = require('request');
 
 // the db
 var db = new sqlite3.Database(file);
+db.exec('PRAGMA foreign_keys = ON');
 
 //favicon
 
@@ -31,16 +32,52 @@ var httpServer = http.createServer(function (req, res) {
             postData = splitQueryString(Buffer.concat(postBody).toString());
             switch (postData.command) {
                 case 'newOvning':
-                    temp = postData.namn.replace(/[^a-zA-Z0-9]+/g, '*');
-                    db.run('INSERT INTO OVNINGAR (NAMN) VALUES(?)', temp);
+                    db.run('INSERT INTO OVNINGAR (NAMN) VALUES(?)', postData.namn, function () {
+                        res.writeHead(302, {
+                            'Location': '/ovningar'
+                            // 'Location': '/ovningar/edit/' + this.lastID
+                        });
+                        res.end();
+                    });
+                    break;
+                case 'editOvning':
+                    db.run('UPDATE OVNINGAR SET NAMN = ? WHERE ID = ?', postData.namn, postData.id, function () {
+                        res.writeHead(302, {
+                            'Location': '/ovningar'
+                        });
+                        res.end();
+                    });
+                    break;
+                case 'newPerson':
+                    db.run('INSERT INTO PERSONER (NAMN) VALUES(?)', postData.namn, function () {
+                        res.writeHead(302, {
+                            'Location': '/personer'
+                        });
+                        res.end();
+                    });
+                    break;
+                case 'editPerson':
+                    db.run('UPDATE PERSONER SET NAMN = ?, AKTIV = ? WHERE ID = ?', postData.namn, ((postData.aktiv === undefined) ? 0 : 1), postData.id, function () {
+                        res.writeHead(302, {
+                            'Location': '/personer'
+                        });
+                        res.end();
+                    });
+                    break;
+                case 'addHistory':
+                    db.run("INSERT INTO HISTORY (PERSON_ID, OVNING_ID, VIKT, ANTAL, DATUM) VALUES(?, ?, ?, ?, datetime('now', 'localtime'))", postData['person_id'], postData['ovning_id'], postData.vikt, postData.antal, function () {
+                        res.writeHead(302, {
+                            'Location': req.headers.referer
+                        });
+                        res.end();
+                    })
+                    break;
+                default:
                     res.writeHead(302, {
                         'Location': req.headers.referer
                     });
-                    break;
-                default:
-
+                    res.end();
             }
-            res.end();
         });
 
     } else {
@@ -61,7 +98,13 @@ var httpServer = http.createServer(function (req, res) {
         } else if (dest === 'include') {
             fileName = params.pop();
             var file = fs.readFileSync('include/' + params.join('/') + '/' + fileName);
-            res.writeHead(200, {'Content-Type': 'plain/text'});
+            var ext = fileName.split('.')[1];
+            var header = 'plain/text';
+
+            if (ext === 'svg') {
+                header = 'image/svg+xml';
+            }
+            res.writeHead(200, {'Content-Type': header});
             res.end(file, 'binary');
             return;
         }
@@ -73,14 +116,45 @@ var httpServer = http.createServer(function (req, res) {
 
         switch (dest) {
             case 'setup':
-            headEnd(res);
             createTables();
             res.write('<h1>Tabeller skapade.</h1><a href="/">Tillbaka</a>');
             res.write('<br><img src="favicon.ico"></img>');
             end(res);
             break;
+            case 'historik':
+                var x;
+                db.all('SELECT * FROM HISTORIK_VIEW ORDER BY DATUM ASC', function (err, rows) {
+                    res.write('<table>');
+                    res.write('<tr>');
+                    for (x in rows[0]) {
+                        res.write('<th>' + x + '</th>');
+                    }
+                    res.write('</tr>');
+                    for (x = 0; x < rows.length; x += 1) {
+                        console.log(rows[x]);
+                        res.write('<tr><td>' + rows[x].Person + '</td><td>' + rows[x].Ovning + '</td><td>' + rows[x].VIKT + '</td><td>' + rows[x].ANTAL + '</td><td>' + rows[x].DATUM + '</td></tr>');
+                    }
+                    res.write('</table>');
+                    end(res);
+                });
+                break;
             case 'ovningar':
-            if (params[0] === undefined || (params[0] === 'ID' && params[1] === undefined)) {
+            if (params[0] === 'new') {
+                readTemplate('createOvning', res);
+                end(res);
+            } else if (params[0] === 'edit') {
+                var id = Number(params[1]);
+                if (!isNaN(id)) {
+                    db.get('SELECT NAMN FROM OVNINGAR WHERE ID = ?', [id], function (err, row) {
+                        read('editOvning', {'ID': id, 'NAMN': row.NAMN}, res);
+                        end(res);
+                    });
+                } else {
+                    res.write('<p>ID <code>' + id + '</code> is not a number.</p>');
+                    end(res);
+                }
+            } else {
+                readTemplate('ovningar', res);
                 db.all("SELECT ID, NAMN FROM OVNINGAR", function(err, rows) {
                     if (rows.length === 0) {
                         res.write('<p>Inga övningar att visa.</p>');
@@ -88,42 +162,80 @@ var httpServer = http.createServer(function (req, res) {
                         res.write('<table>');
                         rows.forEach(function (row) {
                             console.log(row);
-                            res.write("<tr><td>" + row.ID + "</td><td>" + row.NAMN + "</tr>");
+                            res.write("<tr><td>" + row.ID + "</td><td>" + row.NAMN + "</td><td><a class='pointer' href='/ovningar/edit/" + row.ID + "'><i class='me material-icons'>mode_edit</i></a></td></tr>");
                         });
                         res.write('</table>');
                     }
                     end(res);
                 });
-            } else if (params[0] === 'ID' && params[1] !== undefined) {
-                var id = Number(params[1]);
-                if (!isNaN(id)) {
-                    db.all("SELECT ID, NAMN FROM OVNINGAR WHERE ID = " + id, function(err, rows) {
+            }
+                break;
+            case '':
+                res.write('startpage');
+                end(res);
+                break;
+            case 'gym':
+                var check = false, check1 = false, personer = '', ovningar = '', x;
+                db.serialize(function () {
+                    db.all('SELECT ID, NAMN FROM PERSONER WHERE AKTIV = 1', function (err, rows) {
+                        for (x = 0; x < rows.length; x += 1) {
+                            personer += '<option value="' + rows[x].ID + '">' + rows[x].NAMN + '</option>';
+                        }
+                        check = true;
+                        if (check && check1) {
+                            read('addHistory', {PERSONER: personer, OVNINGAR: ovningar}, res);
+                            end(res);
+                        }
+                    });
+                    db.all('SELECT ID, NAMN FROM OVNINGAR', function (err, rows) {
+                        for (x = 0; x < rows.length; x += 1) {
+                            ovningar += '<option value="' + rows[x].ID + '">' + rows[x].NAMN + '</option>';
+                        }
+                        check1 = true;
+                        if (check && check1) {
+                            read('addHistory', {PERSONER: personer, OVNINGAR: ovningar}, res);
+                            end(res);
+                        }
+                    });
+
+                });
+                break;
+            case 'personer':
+                if (params[0] === 'new') {
+                    readTemplate('newPerson', res);
+                    end(res);
+                } else if (params[0] === 'edit') {
+                    id = Number(params[1]);
+                    if (!isNaN(id)) {
+                        db.get('SELECT NAMN, AKTIV FROM PERSONER WHERE ID = ?', id, function (err, row) {
+                            read('editPerson', {ID: id, NAMN: row.NAMN, AKTIV: ((row.AKTIV === 1) ? 'checked' : '')}, res);
+                            end(res);
+                        });
+                    } else {
+                        res.write('<p>ID <code>' + id + '</code> is not a number.</p>');
+                        end(res);
+                    }
+                } else {
+                    readTemplate('personer', res);
+                    db.all("SELECT ID, NAMN, AKTIV FROM PERSONER", function(err, rows) {
                         if (rows.length === 0) {
-                            res.write('<p>Inga övningar att visa.</p>');
+                            res.write('<p>Inga personer registrerade.</p>');
                         } else {
                             res.write('<table>');
                             rows.forEach(function (row) {
-                                res.write("<tr><td>" + row.ID + "</td><td>" + row.NAMN + "</tr>");
+                                console.log(row);
+                                res.write("<tr><td>" + row.ID + "</td><td>" + row.NAMN + "</td>" +
+                                "<td>- " + ((row.AKTIV === 1) ? 'Aktiv' : 'Ej aktiv') + "</td>" +
+                                "<td><a class='pointer' href='/personer/edit/" + row.ID + "'><i class='me material-icons'>mode_edit</i></a></tr>");
                             });
                             res.write('</table>');
                         }
                         end(res);
                     });
-                } else {
-                    res.write('<p>ID <code>' + id + '</code> is not a number.</p>');
-                    end(res);
                 }
-            } else if (params[0] === 'new') {
-                readTemplate('createOvning', res);
-                end(res);
-            } else {
-                res.write('Unhandled param.');
-                end(res);
-            }
-            break;
+                break;
             default:
-            headEnd(res);
-            res.write("<h1>Hej Henrik!</h1>");
+            res.write("<h1>Such 404, very wow.</h1>");
             end(res);
         }
     }
@@ -137,10 +249,7 @@ httpServer.listen(port, function () {
 
 function HTMLStart(res) {
     readTemplate('htmlHead', res);
-}
-
-function headEnd(res) {
-    res.write('</head><body>');
+    readTemplate('header', res);
 }
 
 function splitQueryString(input) {
@@ -148,14 +257,14 @@ function splitQueryString(input) {
     temp = input.split('&');
     for (x = 0; x < temp.length; x += 1) {
         temp2 = temp[x].split('=');
-
-            retObj[temp2[0]] = decodeURIComponent(temp2[1]).replace(/\+/g, ' ');
+        retObj[temp2[0]] = decodeURIComponent(temp2[1]).replace(/\+/g, ' ').replace(/[^a-zA-Z0-9åÅäÄöÖ\s]+/g, '*');
     }
     return retObj;
 }
 
 function end(res) {
-    res.end('</body></html>');
+    readTemplate('footer', res);
+    res.end('</div></body></html>');
 }
 
 function readTemplate(name, res) {
@@ -163,9 +272,33 @@ function readTemplate(name, res) {
     res.write(file, 'binary');
 }
 
+function read(name, replaces, res) {
+    var x, regex, file = fs.readFileSync('include/html/' + name + '.html');
+    file = file.toString();
+    for (x in replaces) {
+        regex = new RegExp('#' + x + '#', 'g');
+        file = file.replace(regex, replaces[x]);
+    }
+    res.write(file);
+}
+
 function createTables() {
     console.log("creating tables");
-    db.run('CREATE TABLE IF NOT EXISTS "PERSON" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "NAMN" VARCHAR NOT NULL  UNIQUE )');
-    db.run('CREATE TABLE IF NOT EXISTS "OVNINGAR" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "NAMN" VARCHAR NOT NULL  UNIQUE )');
-    db.run('CREATE TABLE IF NOT EXISTS "PASS" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "NAMN" VARCHAR NOT NULL  UNIQUE )');
+    db.serialize(function () {
+
+        db.run('CREATE TABLE IF NOT EXISTS "PERSONER" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE ON CONFLICT IGNORE , "NAMN" VARCHAR NOT NULL  UNIQUE ON CONFLICT IGNORE, "AKTIV" INTEGER DEFAULT 1)');
+        db.run('INSERT INTO PERSONER (NAMN) VALUES("Henrik")');
+
+        db.run('CREATE TABLE IF NOT EXISTS "OVNINGAR" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE ON CONFLICT IGNORE , "NAMN" VARCHAR NOT NULL  UNIQUE ON CONFLICT IGNORE )');
+        db.run('INSERT INTO OVNINGAR (NAMN) VALUES("Marklyft")');
+        db.run('INSERT INTO OVNINGAR (NAMN) VALUES("Bänkpress")');
+
+        db.run('CREATE TABLE IF NOT EXISTS "HISTORIK" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "VIKT" DOUBLE NOT NULL , "ANTAL" INTEGER NOT NULL, "PERSON_ID" INTEGER NOT NULL, "OVNING_ID" INTEGER NOT NULL, "DATUM" DATETIME, FOREIGN KEY("PERSON_ID") REFERENCES PERSONER(ID), FOREIGN KEY("OVNING_ID") REFERENCES OVNINGAR(ID))');
+
+        db.run('CREATE VIEW IF NOT EXISTS "historik_view" AS  SELECT p.NAMN AS "Person", o.NAMN AS "Ovning", h.VIKT, h.ANTAL, h.DATUM FROM HISTORIK h JOIN PERSONER p ON h.PERSON_ID = p.ID JOIN OVNINGAR o ON h.OVNING_ID = o.ID');
+
+        db.run('CREATE TABLE IF NOT EXISTS "PASS" ("ID" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE ON CONFLICT IGNORE , "NAMN" VARCHAR NOT NULL  UNIQUE ON CONFLICT IGNORE )', function () {
+            console.log("tables created");
+        });
+    });
 }
