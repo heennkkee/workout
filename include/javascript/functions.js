6496d08dcee9a6c7717e72460923c491dc453c2b
Henrik Aronsson <henrik.aronsson.94@gmail.com>
2017-02-15 19:22:18 +0100

    removed fullscreen click

--- include/javascript/functions.js
+++ include/javascript/functions.js
@@ -22,18 +22,3 @@ function myUrl() {
 function validate(form) {
 
 }
-
-function toggleFullscreen() {
-    var doc = window.document;
-    var docEl = doc.documentElement;
-
-    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-        requestFullScreen.call(docEl);
-    }
-    else {
-        cancelFullScreen.call(doc);
-    }
-}
